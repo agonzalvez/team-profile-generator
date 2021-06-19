ba57a10272eb8d487bdc0fd9835b0c6fef9fc2f0
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-06-18 21:27:04 -0400

    Folders, files, and pseudo created

---
+++ index.js
@@ -0,0 +1,12 @@
+// inquirer asks for manager info
+
+//  ask if user wishes to fill out employee positions
+    // if no, end inquirer and load HTML with fs
+    // if intern, user inquirer intern question set
+    // if engineer, user inquirer engineer question set
+    // after question, ask if they want to add another employee
+    
+// .then 
+    // take answers
+    // pass content into the template
+    // pass template into fs
