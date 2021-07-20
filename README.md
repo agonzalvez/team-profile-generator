f89cac7e4afa503876ba223f9009751c30405401
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-07-20 19:26:16 -0400

    lib files updated. Index.js updated. ReadMe created

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# team-profile-generator
+# Team Profile Generator
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)  
+## Description
+This app will allow the user to quickly create profiles for different team members, their position and role. After the user fills out the questionaire on their terminal, a new HTML will be created dynamically with a clean and clear UI design.
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributors](#contributors)
+- [Testing](#test)
+- [Questions](#questions)
+## Installation
+1) Download the contents of each file and folder. 2) Instal Inquirer package depency. 3) or simply copy the contents of the package.json in the app, and then run "npm i" for the proper dependencies to be installed.
+## Usage
+[Click](https://drive.google.com/file/d/11PajUS_RKGp2vCGpsWRkz2z6UrcOxdLL/view) this link watch video walkthrough for this app. 
+## License
+This application is under the MIT license.  
+## Contributors
+NA.
+## Testing
+This application was tested using Local Browser
+## Questions
+For more information, please visit [GitHub Profile](https://github.com/agonzalvez/).  
+For any questions, you may email me at agonzalvez1@gmail.com.
--- index.js
+++ index.js
@@ -1,134 +1,206 @@
+//require files we will be needing 
+
+//fs is for writing the file
 const fs = require('fs');
-const inquirer = require("inquirer");
-const Manager = require("./lib/manager");
-const Engineer = require("./lib/engineer");
-const Intern = require("./lib/intern");
-const teams = []
 
+//inquirer so that it will ask questions 
+const inquirer = require('inquirer');
+// we need these constructors
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
 
-inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the Manager's name?"
+const team = [];
+
+//function to start questions and start to generate html
+function init() {
+    startGenhtml();
+    addMember();
+}
+
+//this function handles the questions that the user will be prompted with
+function addMember(){
+    inquirer.prompt([{
+        //
+            type: 'input',
+            name: 'name',
+            message: "What is the team member's name?"
         },
         {
-            type: "input",
-            name: "email",
-            message: "What is your email?"
+            type: "list",
+            message: "What is the team member's role?",
+            choices: ["Engineer", "Intern", "Manager"],
+            name: 'position'
         },
         {
-            type: "input",
-            name: "special",
-            message: "What is the contact number?"
+            type: 'input',
+            message: "What is team member's id?",
+            name: 'id',
         },
         {
-            type: "input",
-            name: "id",
-            message: "What is the worker's id?"
+            type: 'input',
+            message: "What is team member's email?",
+            name: 'email',
+        }])
+        //after these series of question are ran .then allows this code to be exectued 
+        //IF Engineer is the position it will ask for github username
+        //ELSE if position is set to manager it will ask for office number
+        //else it will ask for school number since Intern is the only position left 
+    .then(function({name, position, id, email}) {
+        let positionInfo = '';
+        if (position === 'Engineer'){
+            positionInfo = 'Github username';
+        } else if (position === 'Manager'){
+            positionInfo = 'Office number';
+        } else {
+            positionInfo = 'School name';
         }
-    ])
+        inquirer.prompt([{
+            type: 'input',
+            name: 'positionInfo',
+            message: "What is team member's ${positionInfo}",
+        },
+        {
+            type: 'list',
+            message: "Continue adding team member's?",
+            choices: [
+                'yes',
+                'no'
+            ],
+            name:'moreMembers'
+        }])
+        //after being asked if they want to add more members this .then will allow more members to be added 
+        //and sets if else statements that will allow member to be constructed
+        .then(function({positionInfo, moreMembers}) {
+            let newMember;
+            if ( position === 'Engineer') { 
+                newMember = new Engineer (name, id, email, positionInfo);    
+            } else if (position === 'Intern') {
+                newMember = new Intern (name, id, email, positionInfo);
+            } else {
+                newMember = new Manager (name, id, email, positionInfo);
+            }
+            //pushes the newMember into team
+            team.push(newMember);
+            //the addhtml will take in the parameter newMmeber
+            addHtml(newMember)
+            //this .then allows user to add more members else it will run the function finishHTML
+            .then(function() {
+                if (moreMembers === 'yes') {
+                    addMember();
+                } else {
+                    finishHtml();
+                }
+            });
+        });
 
-    .then((answer) => {
-        let manager = new Manager(answer.name, answer.id, answer.email, answer.special);
-        teams.push(manager);
-        anotherOne();
     });
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-        err ? console.error(err) : console.log('Success!'))
-}
+};
 
-function additionalEntry() {
-    inquirer.prompt([
-
-        {
-            type: "checkbox",
-            name: "repeat",
-            message: "Would you like to add another employee?",
-            choices: ["Engineer", "Intern", "None"]
+//this function starts to generates html so adding in the top of html without any user input yet
+function startGenhtml(){
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Employee Tracker</title>
+    </head>
+    <header>
+        <h1> Team Generator</h1>
+    </header>
+    <body>
+        <main>
+            <br>
+            <br>
+            <div class="row">
+                <div class="column">`;
+    fs.writeFile('./dist/log.html', html, function(err) {
+        if (err){
+            console.log(err);
         }
-    ])
-        .then((answer) => {
-            if (answer.repeat.toString() == "None") {
-                let storedCards = "";
-                teams.forEach(element => {
-                    storedCards += generateCards(element)
+    });
+    console.log('start');
+}//fs writes to the file sets the path the type of file and if there is an error it will console log it
 
-                });
-                writeToFile("index.html", generateHTML(storedCards));
+//this function adds the HTML with the member parameter and returns new promise 
+function addHtml(member){
+    return new Promise(function(resolve, reject) {
+        //will allow the methods to be grabbed for the member and set them equal to a const
+        const name = member.getName();
+        const position = member.getPosition();
+        const id = member.getId();
+        const email = member.getEmail();
+        let data = "";
 
-            } else if (answer.repeat.toString() == "Engineer") {
-                inquirer.prompt([
-                    {
-                        type: "input",
-                        name: "name",
-                        message: "What is the Engineer's name?"
-                    },
-                    {
-                        type: "input",
-                        name: "email",
-                        message: "What is your email?"
-                    },
-                    {
-                        type: "input",
-                        name: "id",
-                        message: "What is the worker's id?"
-                    },
-                    {
-                        type: "input",
-                        name: "special",
-                        message: "What is your GitHub username?"
-                    },
-                ])
-                    .then((answer) => {
-                        let engineer = new Engineer(answer.name, answer.id, answer.email, answer.special)
-                        teams.push(engineer);
-                        additionalEntry();
-                    })
-            } else {
-                inquirer.prompt([
-                    {
-                        type: "input",
-                        name: "name",
-                        message: "What is the Intern's name?"
-                    },
-                    {
-                        type: "input",
-                        name: "email",
-                        message: "What is your email?"
-                    },
-                    {
-                        type: "input",
-                        name: "id",
-                        message: "What is the worker's id?"
-                    },
-                    {
-                        type: "input",
-                        name: "special",
-                        message: "Where did you attend school?"
-                    },
-                ])
-                    .then((answer) => {
-                        let intern = new Intern(answer.name, answer.id, answer.email, answer.special)
-                        teams.push(intern);
-                        additionalEntry();
-                    })
-            }
-        })
+        //this is saying if position is Engineer it will grab the members github and the data will add this template code and Add it to html
+        if (position === 'Engineer'){
+            const gitHub = member.getGithub();
+            data = `<div class="col-6">
+            <div class="cardcard mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Engineer</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">GitHub: ${gitHub}</li>
+            </ul>
+            </div>
+        </div>`;
+        //this is saying if position is Intern it will grab the members school and the data will add this template code and Add it to html
+        } else if (position === 'Intern'){
+            const school = member.getSchool();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else {
+        ////this is saying else any other position it will grab the members office number data will add this to the letDATA
+            const officePhone = member.getOfficeNumber();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">Office Phone: ${officePhone}</li>
+            </ul>
+            </div>
+        </div>`
+        }
+        // console.log('new team member added');
+        fs.appendFile("./dist/log.html", data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+    });
+});
+}
+//this is set to finsish the HTML so that if a user does not want to add anymore members it will finsih the html and close it 
+function finishHtml() {
+    const html = ` </div>
+    </div>
+    
+</body>
+</html>`;
+//this appends to the file log.html 
+    fs.appendFile("./dist/log.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("end");
 }
 
-
-// inquirer asks for manager info
-
-//  ask if user wishes to fill out employee positions
-    // if no, end inquirer and load HTML with fs
-    // if intern, user inquirer intern question set
-    // if engineer, user inquirer engineer question set
-    // after question, ask if they want to add another employee
-
-// .then 
-    // take answers
-    // pass content into the template
-    // pass template into fs
+//this calls the function init which starts the series of questions and starts to generate HTML
+init();
